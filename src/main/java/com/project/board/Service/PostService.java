896cd1dfeb529ea9597c7444678371adccbe4551
jeonoseung <114666481+jeonoseung@users.noreply.github.com>
2024-08-18 22:12:56 +0900

    Feat: [추가] 게시글 Service 기능 추가
    데이터 조회 및 게시글 추가 기능

--- src/main/java/com/project/board/Service/PostService.java
+++ src/main/java/com/project/board/Service/PostService.java
@@ -1,8 +1,36 @@
 package com.project.board.Service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.project.board.DTO.CreatePostRequest;
+import com.project.board.Entity.CategoryEntity;
+import com.project.board.Entity.PostEntity;
+import com.project.board.Repo.CategoryRepo;
+import com.project.board.Repo.PostRepo;
+
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor
 public class PostService {
-    
+    private final PostRepo postRepo;
+    private final CategoryRepo categoryRepo;
+
+    public List<PostEntity> GetPostList(){
+        List<PostEntity> posts = postRepo.findAll();
+        return posts;
+    }
+
+    public void CreatePost(CreatePostRequest createPostRequest){
+
+        CategoryEntity category = categoryRepo.findById(createPostRequest.GetPostCategory())
+        .orElseThrow(() -> new IllegalArgumentException("카테고리가 올바르지 않습니다."));
+        PostEntity post = new PostEntity();
+        post.setTitle(createPostRequest.GetPostTitle());
+        post.setContent(createPostRequest.GetPostContent());
+        post.setCategory(category);
+        post.setPost_keyword(createPostRequest.GetPostKeyword());
+    }
 }
