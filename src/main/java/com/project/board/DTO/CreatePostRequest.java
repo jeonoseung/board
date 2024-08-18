42ee53e7cbc49f1af0d0ab752535814ec138fd66
jeonoseung <114666481+jeonoseung@users.noreply.github.com>
2024-08-18 21:58:56 +0900

    Feat: [추가] 게시글 작성 DTO 추가

---
+++ src/main/java/com/project/board/DTO/CreatePostRequest.java
@@ -0,0 +1,37 @@
+package com.project.board.DTO;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public class CreatePostRequest {
+    @NotBlank(message = "게시글 제목을 입력해 주세요.")
+    @Size(min=5,max=50,message="게시글 제목은 최소 5자 ~ 최대 50자까지 입력이 가능합니다.")
+    private String post_title;
+    @NotBlank(message = "게시글 내용을 입력해 주세요.")
+    @Size(min=5,message = "게시글 내용은 최소 5자 이상 입력이 필요합니다.")
+    private String post_content;
+    @NotNull(message="카테고리가 올바르지 않습니다.")
+    private Long post_category;
+    private String[] post_keyword;
+
+    public String GetPostTitle(){
+        return post_title;
+    }
+
+    public String GetPostContent(){
+        return post_content;
+    }
+    public Long GetPostCategory(){
+        return post_category;
+    }
+    public String GetPostKeyword(){
+        if(post_keyword != null && post_keyword.length > 0){
+            return String.join(",",post_keyword);
+        }
+        else {
+            return "";
+        }
+    }
+
+}
