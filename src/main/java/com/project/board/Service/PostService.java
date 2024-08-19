fcf1723adfdd076479c922ea25dc174ac2c75ce5
jeonoseung <114666481+jeonoseung@users.noreply.github.com>
2024-08-19 21:19:56 +0900

    Feat: [추가] 최종적으로 DB에 저장하도록 설정

--- src/main/java/com/project/board/Service/PostService.java
+++ src/main/java/com/project/board/Service/PostService.java
@@ -2,6 +2,7 @@ package com.project.board.Service;
 
 import java.util.List;
 
+import com.project.board.Enum.PostState;
 import org.springframework.stereotype.Service;
 
 import com.project.board.DTO.CreatePostRequest;
@@ -31,6 +32,8 @@ public class PostService {
         post.setTitle(createPostRequest.GetPostTitle());
         post.setContent(createPostRequest.GetPostContent());
         post.setCategory(category);
+        post.setPost_state(PostState.ACTIVE);
         post.setPost_keyword(createPostRequest.GetPostKeyword());
+        postRepo.save(post);
     }
 }
