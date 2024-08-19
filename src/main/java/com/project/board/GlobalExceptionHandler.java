431038e19bc1fcf2b02181c3f96eb82067746dd6
jeonoseung <114666481+jeonoseung@users.noreply.github.com>
2024-08-19 21:19:38 +0900

    Feat: [추가] 에러 메시지가 body에 담기도록 설정

--- src/main/java/com/project/board/GlobalExceptionHandler.java
+++ src/main/java/com/project/board/GlobalExceptionHandler.java
@@ -1,10 +1,18 @@
 package com.project.board;
 
+import lombok.Setter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+class SetBody {
+    public String errorMessage = "에러가 발생했습니다.";
+    public void SetErrorMessage(String message){
+        this.errorMessage = message;
+    }
+}
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -15,8 +23,11 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<String> handleValidationExceptions(MethodArgumentNotValidException ex) {
-        String errorMessage = ex.getBindingResult().getFieldError().getDefaultMessage();
-        return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
+    public ResponseEntity<SetBody> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        String message = ex.getBindingResult().getFieldError().getDefaultMessage();
+        SetBody bodys = new SetBody();
+        bodys.SetErrorMessage(message);
+        System.out.println(bodys);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(bodys);
     }
 }
