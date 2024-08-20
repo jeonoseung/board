42a672fb4098841bb449f9632df4352ea3106a63
jeonoseung <114666481+jeonoseung@users.noreply.github.com>
2024-08-20 21:20:39 +0900

    Fix: [수정] 게시글 저장 기능 적용 및 Response값 적용

--- src/main/java/com/project/board/APIController.java
+++ src/main/java/com/project/board/APIController.java
@@ -1,18 +1,31 @@
 package com.project.board;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.board.DTO.CreatePostRequest;
+import com.project.board.Service.PostService;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PostMapping;
+
 
 
 @RestController
 @RequiredArgsConstructor
 public class APIController {
 
-    @GetMapping("/post/wirte")
-    public void postMethodName(){
-        // postService.InsertPost();
+    private final PostService postService;
+
+    @PostMapping("/post/wirte")
+    public ResponseEntity<Void> CreatePost(@Valid @RequestBody CreatePostRequest createPostRequest){
+        postService.CreatePost(createPostRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
 
