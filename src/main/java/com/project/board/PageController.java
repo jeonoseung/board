2d27d0c61d7677285706a9ce456bb7d03628629f
jeonoseung <114666481+jeonoseung@users.noreply.github.com>
2024-08-20 21:19:58 +0900

    Feat: [추가] ModelAndView 적용 및 게시글 데이터 전달

--- src/main/java/com/project/board/PageController.java
+++ src/main/java/com/project/board/PageController.java
@@ -1,8 +1,30 @@
 package com.project.board;
 
+import com.project.board.Entity.PostEntity;
+import com.project.board.Service.PostService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
+@RequiredArgsConstructor
 public class PageController {
+    
+    private final PostService postService;
+
+    @RequestMapping("/")
+    public ModelAndView ViewMainPage(ModelAndView mv){
+        List<PostEntity> posts = postService.GetPostList();
+
+        mv.setViewName("layout");
+        mv.addObject("url","index.jsp");
+        mv.addObject("post",posts);
+
+        return mv;
+    }
+
     
 }
