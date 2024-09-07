ae50e084e95bca8b8266873bf4fcdb9db0264acb
jeonoseung <114666481+jeonoseung@users.noreply.github.com>
2024-09-08 04:31:10 +0900

    Feat: [추가] 게시글 목록 DTO

---
+++ src/main/java/com/project/board/DTO/QuerInterface/PostListView.java
@@ -0,0 +1,16 @@
+package com.project.board.DTO.QuerInterface;
+
+import java.time.LocalDateTime;
+
+public interface PostListView {
+   
+    public Long getPost_pid();
+    public String getPost_title();
+    public String getPost_content();
+    public LocalDateTime getPost_create_date();
+    public String getUser_nickname();
+    public String getCategory_name();
+    public int getComment_count();
+    public int getRec_count();
+    public int getPost_view();
+}
