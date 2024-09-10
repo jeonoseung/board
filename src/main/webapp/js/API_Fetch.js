a1ac5807ca24c451f9845ae1ae16cc9b7c8c749a
jeonoseung <114666481+jeonoseung@users.noreply.github.com>
2024-09-11 04:37:07 +0900

    Feat: [추가] delete fetch 함수 추가

--- src/main/webapp/js/API_Fetch.js
+++ src/main/webapp/js/API_Fetch.js
@@ -60,4 +60,31 @@ class Fetch {
             }
         }
     }
+    static async DELETE({ url,body }){
+        const res = await fetch(url,{
+            method:"DELETE",
+            headers:{
+                'Content-Type': 'application/json'
+            },
+            cache:"no-store",
+            credentials:"include",
+            body:JSON.stringify(body)
+        })
+        const content_type = res.headers.get('Content-Type')
+        if(res.ok){
+            if(content_type === null){
+                return ""
+            }
+            else {
+                return await res.json()
+            }
+        }
+        else {
+            const json = await res.json()
+            throw {
+                message:json.errorMessage,
+                status:res.status
+            }
+        }
+    }
 }
